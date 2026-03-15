8820235b27826751c1018ea5dd75e1707ec1722e
mattynunchucks <mattniemczyk@gmail.com>
2026-03-14 21:35:16 -0400

    and here we go

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/cosmogenesis/",
   define: {
     __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
   },
